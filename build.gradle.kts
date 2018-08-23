66c14ce4ab17165a2afee1780dab47d87c265ccc
Adam Poloczek <piratjsk@gmail.com>
2018-08-23 10:19:59 +0200

    setup project

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+	java
+}
+
+repositories {
+	jcenter()
+	maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+	compile("org.bukkit:bukkit:1.13-R0.1-SNAPSHOT")
+}
